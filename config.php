e506d5f69e4cc9b59d58affd0ba6f93ed94c64a8
dschaefer-aartech <dschaefer@aartech.de>
2024-10-19 20:28:15 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 	error_reporting(E_ALL);
 	
-	define ( 'MYSQL_HOST',	'ms2777.gamedata.io' );
-	define ( 'MYSQL_USER', 'ni1971828_2_DB' );
-	define ( 'MYSQL_PASSWORD', 'vHHvkJR9' );
-	define ( 'MYSQL_DATABASE', 'ni1971828_2_DB' );
+	define ( 'MYSQL_HOST',	'xxx' );
+	define ( 'MYSQL_USER', 'xxx' );
+	define ( 'MYSQL_PASSWORD', 'xxx' );
+	define ( 'MYSQL_DATABASE', 'xxx' );
 
 ?>
